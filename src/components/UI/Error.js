dc35f9a77247c24343506fdb0610617d1501dd87
Ashish7520 <ashishnandwana2@gmail.com>
2023-09-20 23:25:23 +0530

    using portal for better code experience

--- src/components/UI/Error.js
+++ src/components/UI/Error.js
@@ -1,24 +1,45 @@
 import React from "react";
+import ReactDOM from "react-dom";
 
-import './Error.css'
+import "./Error.css";
 
-const Error = (props) =>{
-    return (
-        <div className="outer">
-            <div className="backdrop" onClick={props.onConfirm} />
-            <div className="modal">
-            <header>
-                <h2>{props.title}</h2>
-            </header>
-         
-                <p>{props.massage}</p>
-           
-            <footer>
-                <button onClick={props.onConfirm}>Okay</button>
-            </footer>
-            </div>
-        </div>
-    )
-}
+const Backdrops = (props) => {
+  return <div className="backdrop" onClick={props.onConfirm} />;
+};
+
+const ErrorModel = (props) => {
+  return (
+    <div className="modal">
+      <header>
+        <h2>{props.title}</h2>
+      </header>
+
+      <p>{props.massage}</p>
+
+      <footer>
+        <button onClick={props.onConfirm}>Okay</button>
+      </footer>
+    </div>
+  );
+};
+
+const Error = (props) => {
+  return (
+    <div className="outer">
+      {ReactDOM.createPortal(
+        <Backdrops onConfirm={props.onConfirm} />,
+        document.getElementById("root-backdrops")
+      )}
+      {ReactDOM.createPortal(
+        <ErrorModel
+          title={props.title}
+          massage={props.massage}
+          onConfirm={props.onConfirm}
+        />,
+        document.getElementById("root-models")
+      )}
+    </div>
+  );
+};
 
 export default Error;
